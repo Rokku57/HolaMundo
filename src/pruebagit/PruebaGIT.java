2ddfefdff2a64f0cb196d3844b725b20ea4cd57c
Rokku57 <83916781+Rokku57@users.noreply.github.com>
2021-05-09 11:02:08 -0500

    Update PruebaGIT.java

--- src/pruebagit/PruebaGIT.java
+++ src/pruebagit/PruebaGIT.java
@@ -15,7 +15,7 @@ public class PruebaGIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HOla mundo");
+        System.out.println("HOla universo");
     }
     
 }
